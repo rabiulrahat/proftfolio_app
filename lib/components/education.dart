d4c7ee6ab485b3a10b10d1d2130dbf6168769cb6
rabiulrahat <rabiulrahat0@gmail.com>
2024-07-15 23:25:53 +0600

    "added data model "

--- lib/components/education.dart
+++ lib/components/education.dart
@@ -1,9 +1,7 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:timelines/timelines.dart';
+import '../data_model/paper_datamodel.dart';
 
 class EducationPage extends StatefulWidget {
   const EducationPage({super.key});
@@ -13,6 +11,26 @@ class EducationPage extends StatefulWidget {
 }
 
 class _EducationPageState extends State<EducationPage> {
+  List<ResearchPaper> papers = [
+    ResearchPaper(
+        date: "September 2018",
+        paperName:
+            "Design of Reversible Shift Registers Minimizing Number of Gates, Constant Inputs and Garbage Outputs",
+        journalName: "IEEE Xplore",
+        journalType: "Conference"),
+    ResearchPaper(
+        date: "September 2021",
+        paperName:
+            "Future Friend Recommendation System based on User Similarities in Large-Scale on Social Network",
+        journalName: "IJACSA",
+        journalType: "Journal"),
+    ResearchPaper(
+        date: "October 2021",
+        paperName:
+            "Deep Viewing for Covid-19 Detection from X-Ray Using CNN Based Architecture",
+        journalName: "EECSI",
+        journalType: "Conference"),
+  ];
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -37,18 +55,37 @@ class _EducationPageState extends State<EducationPage> {
             shrinkWrap: true,
             scrollDirection: Axis.vertical,
             builder: TimelineTileBuilder.fromStyle(
-                itemCount: 10,
+                itemCount: papers.length,
                 contentsAlign: ContentsAlign.alternating,
                 contentsBuilder: (context, index) {
-                  return const Card(
+                  return Card(
                     child: Padding(
                       padding: EdgeInsets.all(8.0),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text('Sep 2018'),
-                          Text('Design of Reversible Shift Registers Minimizing Number of Gates, Constant Inputs and Garbage Outputs'),
-                          Text('2 April 2020'),
+                          Text(
+                            papers[index].date,
+                            style: const TextStyle(
+                                color: Colors.black87,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w300),
+                          ),
+                          Text(
+                            papers[index].journalName,
+                            style: const TextStyle(
+                                color: Colors.black87,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            papers[index].journalType,
+                            style: const TextStyle(
+                                color: Colors.black87,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w900),
+                          ),
+                          Text(papers[index].paperName),
                         ],
                       ),
                     ),
---
+++ lib/data_model/paper_datamodel.dart
@@ -0,0 +1,11 @@
+class ResearchPaper {
+  late String date;
+  late String paperName;
+  late String journalName;
+  late String journalType;
+  ResearchPaper(
+      {required this.date,
+      required this.paperName,
+      required this.journalName,
+      required this.journalType});
+}
